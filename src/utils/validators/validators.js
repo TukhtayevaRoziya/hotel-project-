6e5cfb029f2e4c28cae67305b65cde018c4e0546
roziya2008 <muhammadzodaumar2002@gmail.com>
2021-06-15 21:27:59 +0500

    "responsive"

--- src/utils/validators/validators.js
+++ src/utils/validators/validators.js
@@ -2,7 +2,7 @@ export const required = value => {
     if (value) return undefined;
     return "Fill in the field!";
 }
-export const maxlengthCreator =(maxlength) => (value) => {
+export const maxlengthCreator = (maxlength) => (value) => {
     if (value.length > maxlength) return `Max length is ${maxlength} sybols!`;
     return undefined;
 }
